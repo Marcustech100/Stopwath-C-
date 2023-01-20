f88d161389a5a2d0906e5290a90bbb9c71e85d9e
Marcustech100 <mvtechnologic@gmail.com>
2023-01-20 20:53:32 -0300

    criando estrutura, e usando Thread e Sleep

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+           start(); 
+        }
+
+        static void start()
+        {
+            int time = 10;
+            int currenTime = 0;
+            
+            while(currenTime != time)
+            {
+                Console.Clear();
+                currenTime ++;
+                Console.WriteLine(currenTime);
+                Thread.Sleep(1000);    
+            }
+        }
+    }
+}
